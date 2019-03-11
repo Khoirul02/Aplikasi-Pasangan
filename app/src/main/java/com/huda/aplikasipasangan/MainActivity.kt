3318978e5d4603848c0a86eb970166a21d41c91e
Khoirul02 <hudak006@gmail.com>
2019-03-11 14:22:39 +0700

    pasangan foto

--- app/src/main/java/com/huda/aplikasipasangan/MainActivity.kt
+++ app/src/main/java/com/huda/aplikasipasangan/MainActivity.kt
@@ -13,18 +13,22 @@ class MainActivity : AppCompatActivity() {
         btn_cari.setOnClickListener { cari() }
     }
     private fun cari(){
-        val randomPria = Random().nextInt(3)+1
-        val randomWanita = Random().nextInt(3)+1
+        val randomPria = Random().nextInt(5)+1
+        val randomWanita = Random().nextInt(5)+1
         val drawableResPria = when (randomPria){
-            1-> R.drawable.pria_1
-            2-> R.drawable.pria_2
-            3-> R.drawable.pria_3
+            1-> R.drawable.huda_mobile
+            2-> R.drawable.filipus_mobile
+            3-> R.drawable.edi_mobile
+            4-> R.drawable.pria_1
+            5-> R.drawable.pria_2
             else -> R.drawable.pasangan
         }
         val drawableResWanita = when (randomWanita){
-            1-> R.drawable.wanita_1
-            2-> R.drawable.wanita_2
-            3-> R.drawable.wanita_3
+            1-> R.drawable.fuji_mobile
+            2-> R.drawable.hanum_mobile
+            3-> R.drawable.wanita_1
+            4-> R.drawable.wanita_2
+            5-> R.drawable.wanita_3
             else-> R.drawable.pasangan
         }
         foto_pria.setImageResource(drawableResPria)
